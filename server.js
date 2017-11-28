29c551ead5be12c6304e7d53985bc13bebd1deea
James C. Hurst <jchurstmail@gmail.com>
2017-11-28 16:23:04 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// call packages
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+var routes = require('./app/routes/index.js');
+var api = require('./app/api/timestamp.js');
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/public', express.static(process.cwd() + '/public'));
+    
+var port = process.env.PORT || 8080; // set port
+       
+routes(app);
+api(app);
+
+app.listen(port, function() {
+    console.log('Node.js listening on port ' + port);
+});
