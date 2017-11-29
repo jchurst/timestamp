2a82c116d2dd7aa572059de111a4cc4e3c4b56d1
James C. Hurst <jchurstmail@gmail.com>
2017-11-29 15:27:19 -0500

    Create api.js

---
+++ routes/api.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+// modules this api provides routing for
+const timestamp = require('../timestamp/timestamp');
+
+// Timestamp Microservice Routes
+router.route('/timestamp/api/v1/:query').get(timestamp.parse);
+router.route('/timestamp/api/:query').get(timestamp.parse);
+router.route('/timestamp/:query').get(timestamp.parse);
+
+// If not an api request, display the index page
+router.get('*', (req, res) => {
+  const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  res.render('index.html', {
+    fullUrl: fullUrl,
+    title: 'Timestamp Microservice'
+  });
+});
+
+module.exports = router;
